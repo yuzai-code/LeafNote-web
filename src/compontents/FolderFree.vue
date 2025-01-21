5c63b0de8e5afd2e0eeb234161014ebf8946e804
yuzai <958684803@qq.com>
2025-01-21 17:12:10 +0800

    style: 修改子文件夹名称样式

--- src/compontents/FolderFree.vue
+++ src/compontents/FolderFree.vue
@@ -97,7 +97,7 @@
                     d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
                   />
                 </svg>
-                <span class="text-sm">{{ child.name }}</span>
+                <span class="text-sm flex-1">{{ child.name }}</span>
                 <FolderItem />
               </div>
             </div>
