e04d4d3d744acf7f8fd6842360068df0f5d150d2
yuzai <958684803@qq.com>
2025-01-21 17:05:50 +0800

    refactor: 将添加按钮和更多操作按钮提取到FolderItem组件

--- src/compontents/FolderFree.vue
+++ src/compontents/FolderFree.vue
@@ -54,20 +54,7 @@
             />
           </svg>
           <span class="text-sm flex-1">{{ folder.name }}</span>
-          
-          <!-- 添加按钮 -->
-          <button class="btn btn-ghost btn-xs -mr-1" @click.stop>
-            <svg class="h-4" viewBox="0 0 24 24" fill="none" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/>
-            </svg>
-          </button>
-
-          <!-- 更多操作按钮 -->
-          <button class="btn btn-ghost btn-xs -ml-1" @click.stop>
-            <svg class="h-4" viewBox="0 0 24 24" fill="none" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01" />
-            </svg>
-          </button>
+          <FolderItem />
         </div>
         <!-- 子文件夹 -->
         <div v-if="folder.expanded && folder.children?.length" class="mt-1">
@@ -111,6 +98,7 @@
                   />
                 </svg>
                 <span class="text-sm">{{ child.name }}</span>
+                <FolderItem />
               </div>
             </div>
           </template>
@@ -123,6 +111,7 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
 import { ApiService, Category } from "../api";
+import FolderItem from "./FolderItem.vue";
 
 // 目录列表状态
 const folders = ref<Category[]>([]);
---
+++ src/compontents/FolderItem.vue
@@ -0,0 +1,30 @@
+<template>
+        <!-- 添加按钮 -->
+        <button class="btn btn-ghost btn-xs -mr-1" @click.stop>
+            <svg class="h-4" viewBox="0 0 24 24" fill="none" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/>
+            </svg>
+          </button>
+          <div class="dropdown">
+          <!-- 更多操作按钮 -->
+          <button class="btn btn-ghost btn-xs -ml-1" @click.stop>
+            <svg class="h-4" viewBox="0 0 24 24" fill="none" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01" />
+            </svg>
+          </button>
+          <ul class="menu dropdown-content bg-base-100 rounded-box z-[1] min-w-[7rem] whitespace-nowrap p-1 shadow">
+            <li><a>新建笔记</a></li>
+            <li><a>新建目录</a></li>
+            <li><a>重命名</a></li>
+            <li><a>删除</a></li>
+          </ul>
+  </div>
+</template>
+    
+<script setup lang='ts'>
+    
+</script>
+    
+<style>
+    
+</style>
