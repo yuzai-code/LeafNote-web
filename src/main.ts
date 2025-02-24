49f8e26972c0600d763589d34a166c8e17586cf7
yuzai <958684803@qq.com>
2025-02-24 17:07:41 +0800

    feat: 添加primevue组件库

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./router";
 import "@/styles/main.css";
+import PrimeVue from "primevue/config";
+import Aura from '@primeuix/themes/aura';
 
 const app = createApp(App);
 app.use(router);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+})
 app.mount("#app");
